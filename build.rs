8d280d2a64c38fc7ac0ddd55f304bd58ee155bbf
meowtec <bertonzh@gmail.com>
2023-12-29 22:48:03 +0800

    feat: x265 -> kvazaar

--- build.rs
+++ build.rs
@@ -1,4 +1,7 @@
-use std::path::{Path, PathBuf};
+use std::{
+    fs,
+    path::{Path, PathBuf},
+};
 
 fn write_file(path: &Path, content: &str) -> () {
     if let Some(parent) = path.parent() {
@@ -27,7 +30,7 @@ fn main() {
     // #[cfg(feature = "use-vendor")]
     {
         // cmake & pkg_config have some problem when target to wasm
-        if is_wasm {
+        if is_wasm && false {
             let libde265_sources = vec![
                 "alloc_pool.cc",
                 "bitstream.cc",
@@ -76,6 +79,81 @@ fn main() {
                 .include("libde265/extra")
                 .define("HAVE_POSIX_MEMALIGN", "1")
                 .compile("de265");
+
+            let kvazaar_sources = vec![
+                "bitstream.c",
+                "cabac.c",
+                "cfg.c",
+                "checkpoint.c",
+                "constraint.c",
+                "context.c",
+                "cu.c",
+                "encode_coding_tree.c",
+                "encoder.c",
+                "encoder_state-bitstream.c",
+                "encoder_state-ctors_dtors.c",
+                "encoder_state-geometry.c",
+                "encoderstate.c",
+                "fast_coeff_cost.c",
+                "filter.c",
+                "image.c",
+                "imagelist.c",
+                "input_frame_buffer.c",
+                "inter.c",
+                "intra.c",
+                "kvazaar.c",
+                "ml_classifier_intra_depth_pred.c",
+                "ml_intra_cu_depth_pred.c",
+                "nal.c",
+                "rate_control.c",
+                "rdo.c",
+                "sao.c",
+                "scalinglist.c",
+                "search.c",
+                "search_inter.c",
+                "search_intra.c",
+                "strategyselector.c",
+                "tables.c",
+                "threadqueue.c",
+                "transform.c",
+                "videoframe.c",
+                "yuv_io.c",
+                "strategies/strategies-dct.c",
+                "strategies/strategies-intra.c",
+                "strategies/strategies-nal.c",
+                "strategies/strategies-quant.c",
+                "strategies/strategies-encode.c",
+                "strategies/strategies-ipol.c",
+                "strategies/strategies-picture.c",
+                "strategies/strategies-sao.c",
+                "strategies/avx2/dct-avx2.c",
+                "strategies/avx2/encode_coding_tree-avx2.c",
+                "strategies/avx2/intra-avx2.c",
+                "strategies/avx2/ipol-avx2.c",
+                "strategies/avx2/picture-avx2.c",
+                "strategies/avx2/quant-avx2.c",
+                "strategies/avx2/sao-avx2.c",
+                "strategies/altivec/picture-altivec.c",
+                "strategies/generic/dct-generic.c",
+                "strategies/generic/encode_coding_tree-generic.c",
+                "strategies/generic/intra-generic.c",
+                "strategies/generic/ipol-generic.c",
+                "strategies/generic/nal-generic.c",
+                "strategies/generic/picture-generic.c",
+                "strategies/generic/quant-generic.c",
+                "strategies/generic/sao-generic.c",
+                "strategies/sse2/picture-sse2.c",
+                "strategies/sse41/picture-sse41.c",
+                "strategies/x86_asm/picture-x86-asm.c",
+                "extras/getopt.c",
+                "extras/libmd5.c",
+            ];
+
+            cc::Build::new()
+                .warnings(false)
+                .files(kvazaar_sources.iter().map(|s| format!("kvazaar/src/{}", s)))
+                .include("kvazaar/src")
+                .compile("kvazaar");
 
             let x265_sources = vec![
                 "common/primitives.cpp",
@@ -146,11 +224,7 @@ fn main() {
                 // NOT support HIGH_BIT_DEPTH
                 .define("HIGH_BIT_DEPTH", "0")
                 .define("X265_DEPTH", "8")
-                .files(
-                    x265_sources
-                        .iter()
-                        .map(|s| format!("x265/source/{}", s)),
-                )
+                .files(x265_sources.iter().map(|s| format!("x265/source/{}", s)))
                 .include(out_dir.join("x265"))
                 .include("x265/source")
                 .include("x265/source/common")
@@ -192,6 +266,7 @@ fn main() {
                 "plugins/encoder_mask.cc",
                 "plugins/decoder_libde265.cc",
                 "plugins/encoder_x265.cc",
+                "plugins/encoder_kvazaar.cc",
             ];
 
             write_file(
@@ -208,7 +283,8 @@ fn main() {
                 .std("c++11")
                 .define("HAVE_VISIBILITY", "1")
                 .define("HAVE_LIBDE265", "1")
-                .define("HAVE_X265", "1")
+                .define("HAVE_X265", "0")
+                .define("HAVE_KVAZAAR", "1")
                 .files(
                     libheif_sources
                         .iter()
@@ -219,6 +295,7 @@ fn main() {
                 .include("libde265")
                 .include("libde265/extra")
                 .include("x265/source")
+                .include("kvazaar/src")
                 .include("libheif")
                 .compile("heif");
 
@@ -271,32 +348,64 @@ fn main() {
                 None
             };
 
-            let mut x265 = cmake::Config::new("x265/source");
+            #[allow(unused_mut)]
+            #[allow(unused_assignments)]
+            let mut x265_output: Option<PathBuf> = None;
 
-            x265.out_dir(out_dir.join("x265"))
-                .define("ENABLE_SDL", "OFF")
-                .define("BUILD_SHARED_LIBS", "OFF")
-                .define("ENABLE_SHARED", "OFF")
-                .define("ENABLE_ENCODER", "OFF");
+            #[cfg(feature = "use-x265")]
+            {
+                let mut x265 = cmake::Config::new("x265/source");
 
-            if is_wasm {
-                x265.define("ENABLE_ASSEMBLY", "OFF");
+                x265.out_dir(out_dir.join("x265"))
+                    .define("ENABLE_SDL", "OFF")
+                    .define("BUILD_SHARED_LIBS", "OFF")
+                    .define("ENABLE_SHARED", "OFF")
+                    .define("ENABLE_ENCODER", "OFF");
+
+                if is_wasm {
+                    x265.define("ENABLE_ASSEMBLY", "OFF");
+                }
+
+                x265_output = Some(x265.build());
             }
 
-            let x265_output = x265.build();
+            #[allow(unused_mut)]
+            #[allow(unused_assignments)]
+            let mut kvazaar_output: Option<PathBuf> = None;
+
+            #[cfg(not(feature = "use-x265"))]
+            {
+                let mut kvazaar = autotools::Config::new("kvazaar");
+                let kvazaar_out_dir = out_dir.join("kvazaar");
+                fs::create_dir_all(&kvazaar_out_dir).unwrap();
+
+                kvazaar
+                    .out_dir(kvazaar_out_dir)
+                    .reconf("-if")
+                    .enable_static()
+                    .disable_shared();
+                if is_wasm {
+                    kvazaar.disable("asm", None);
+                }
+                kvazaar_output = Some(kvazaar.build());
+            }
 
             let output_paths = [
                 Some(&libde265_output),
-                Some(&x265_output),
+                x265_output.as_ref(),
+                kvazaar_output.as_ref(),
                 libwebp_output.as_ref(),
             ]
             .iter()
             .filter_map(|x| x.to_owned())
             .collect::<Vec<_>>();
 
-            let output = cmake::Config::new("libheif")
+            let mut libheif_config = cmake::Config::new("libheif");
+
+            libheif_config
                 .out_dir(out_dir.join("libheif"))
                 .configure_arg("--preset=release-noplugins")
+                .cxxflag("-Wno-error=uninitialized")
                 .define("BUILD_SHARED_LIBS", "OFF")
                 .define(
                     "CMAKE_PREFIX_PATH",
@@ -306,13 +415,41 @@ fn main() {
                         .collect::<Vec<_>>()
                         .join(";"),
                 )
+                // they are need for emscripten build
+                .define("LIBDE265_INCLUDE_DIR", libde265_output.join("include"))
+                .define("LIBDE265_LIBRARY", "-de265")
+
+                // options
                 .define("WITH_AOM_DECODER", "OFF")
                 .define("WITH_AOM_ENCODER", "OFF")
                 .define("WITH_JPEG_DECODER", "OFF")
                 .define("WITH_JPEG_ENCODER", "OFF")
                 .define("WITH_EXAMPLES", "OFF")
-                .define("WITH_DEFLATE_HEADER_COMPRESSION", "OFF")
-                .build();
+                .define("WITH_DEFLATE_HEADER_COMPRESSION", "OFF");
+
+            if let Some(ref x265_output) = x265_output {
+                libheif_config
+                    .define("X265_INCLUDE_DIR", x265_output.join("include"))
+                    .define("X265_LIBRARY", "-x265")
+                    .define("WITH_X265", "ON")
+                    .define("WITH_KVAZAAR", "OFF")
+                    .define("WITH_KVAZAAR_PLUGIN", "OFF");
+            }
+
+            if let Some(ref kvaazar_output) = kvazaar_output {
+                libheif_config
+                    .define("KVAZAAR_INCLUDE_DIR", kvaazar_output.join("include"))
+                    .define("KVAZAAR_LIBRARY", "-kvazaar")
+                    .define("WITH_X265", "OFF")
+                    .define("WITH_KVAZAAR", "ON")
+                    .define("WITH_KVAZAAR_PLUGIN", "ON");
+            }
+
+            if is_wasm {
+                libheif_config.define("ENABLE_MULTITHREADING_SUPPORT", "OFF");
+            }
+
+            let output = libheif_config.build();
 
             println!("cargo:warning=output: {:?}", output);
 
